78b9292512e4ccacee1c5c346fb792a849c11a9f
Neyran <maxnezerplay@gmail.com>
2025-11-04 20:12:31 +0300

    Фиксы апстрима

---
+++ Content.Server.Database/Migrations/Sqlite/20250701160318_AddCDProfileWidth.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Content.Server.Database.Migrations.Sqlite
+{
+    /// <inheritdoc />
+    public partial class AddCDProfileWidth : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "width",
+                table: "cdprofile",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "width",
+                table: "cdprofile");
+        }
+    }
+}
