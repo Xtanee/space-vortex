a4d90dc0db3b8b907139c693a58fdd3ef1e4ed74
Neyran <maxnezerplay@gmail.com>
2025-11-10 06:36:26 +0300

    Изменения прав доступа для панели выдачи времени

--- Content.Goobstation.Server/Actions/Commands/TimeTransferPanelCommand.cs
+++ Content.Goobstation.Server/Actions/Commands/TimeTransferPanelCommand.cs
@@ -12,7 +12,7 @@ using Robust.Shared.Console;
 
 namespace Content.Goobstation.Server.Actions.Commands;
 
-[AdminCommand(AdminFlags.Moderator)]
+[AdminCommand(AdminFlags.FullAdmin)]
 public sealed class TimeTransferPanelCommand : LocalizedCommands
 {
     [Dependency] private readonly EuiManager _euis = default!;
